b16a4a51f4391a7cc69144a1042ce7a9a318442c
niaw junior <niawkung2@gmail.com>
2017-05-30 16:51:22 +0700

    Add Quiz

---
+++ src/Quiz.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Quiz extends Component{
+
+    render(){
+        return(
+            <div className="quiz">
+                <div className="quiz-content">
+                    <p className="question">What is the sum of <span className="text-info">5</span> and <span className="text-info">5</span>?</p>
+                    <div className="fields"><div className="field-block">10</div></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Quiz;
