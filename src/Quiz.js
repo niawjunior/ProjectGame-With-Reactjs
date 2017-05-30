bd4bb5970c147be3f52e064f2df049faedfd5cd3
niaw junior <niawkung2@gmail.com>
2017-05-30 16:59:43 +0700

    change font

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,8 +6,16 @@ class Quiz extends Component{
         return(
             <div className="quiz">
                 <div className="quiz-content">
-                    <p className="question">What is the sum of <span className="text-info">5</span> and <span className="text-info">5</span>?</p>
+                    <p className="question">ข้อใดคือผลลัพธ์ของ <span className="text-info">5</span> บวก <span className="text-info">5</span>?</p>
+                    <div className="options">
                     <div className="fields"><div className="field-block">10</div></div>
+                    <div className="fields"><div className="field-block">10</div></div>
+                    <div className="fields"><div className="field-block">10</div></div>
+                    <div className="fields"><div className="field-block">10</div></div>
+                </div>
+            </div>
+                <div className="play-again">
+                    <a className="button">เล่นอีกครั้ง</a>
                 </div>
             </div>
         );
