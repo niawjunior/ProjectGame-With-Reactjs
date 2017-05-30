af118bd919ba40384236df8a6c077a0188911744
niaw junior <niawkung2@gmail.com>
2017-05-30 16:41:39 +0700

    Add style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Quize from './Quiz';
 class App extends Component {
   render() {
     return (
@@ -13,7 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+       <Quiz />
       </div>
+
     );
   }
 }
